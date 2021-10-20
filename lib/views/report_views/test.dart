8747636174c50ce0257b6ef56d8ac63eefc6de1d
abbas yazdanmehr <77411860+abbasyazdanmehr@users.noreply.github.com>
2021-10-20 13:50:37 +0330

    fix gitignore.

---
+++ lib/views/report_views/test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ReportPageTest extends StatefulWidget {
+  @override
+  _ReportPageTestState createState() => _ReportPageTestState();
+}
+
+class _ReportPageTestState extends State<ReportPageTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('''
+      '''),
+      ),
+    );
+  }
+}
