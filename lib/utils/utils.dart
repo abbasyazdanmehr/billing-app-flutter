3ee7b1630f694513728033dc2c7c5767a4002ce4
abbas yazdanmehr <77411860+abbasyazdanmehr@users.noreply.github.com>
2022-02-07 15:45:23 +0330

    little bug fixed.

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,9 +1,9 @@
 String getTurnoverType(int index) {
   if (index == 0) {
     return "Income";
-  } else if (index == 2) {
+  } else if (index == 1) {
     return "Cost";
-  } else if (index == 3) {
+  } else if (index == 2) {
     return "FutureCost";
   } else {
     return "All";
