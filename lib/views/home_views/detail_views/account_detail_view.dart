4c71e6495fd97f1b6e5693c4aa27f8584a979fb8
abbas yazdanmehr <77411860+abbasyazdanmehr@users.noreply.github.com>
2022-02-09 10:32:03 +0330

    account and bbill detail view ui changed.

--- lib/views/home_views/detail_views/account_detail_view.dart
+++ lib/views/home_views/detail_views/account_detail_view.dart
@@ -2,6 +2,7 @@ import 'package:billing_app/constants/constants.dart';
 import 'package:billing_app/controllers/bank_accounts_controller.dart';
 import 'package:billing_app/views/home_views/list_views/turnovers_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'package:get/get.dart';
 
 class AccountDetailView extends StatelessWidget {
@@ -11,44 +12,80 @@ class AccountDetailView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     block() {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      return Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Index: $index'),
-              Text('Id: ${controller.bankAccounts[index].id}')
+              Text(
+                'Name: ${controller.bankAccounts[index].name}',
+                style: TextStyle(
+                  fontSize: 22.sp,
+                  fontFamily: "serief",
+                ),
+              ),
+              Text(
+                'Balance: ${controller.bankAccounts[index].balance}' + ' \$',
+                style: TextStyle(fontSize: 22.sp, fontFamily: "serief"),
+              ),
+              Text(
+                'Created: ${controller.bankAccounts[index].created.toString().substring(0, 19)}',
+                style: TextStyle(
+                  fontSize: 16.sp,
+                ),
+              ),
+              Text(
+                  'Description: ${controller.bankAccounts[index].description}'),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: InkWell(
+                  onTap: () {
+                    Get.to(
+                      () => TurnoversListView(
+                        bankAccountId: controller.bankAccounts[index].id,
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'See Turnovers',
+                    style: TextStyle(
+                        fontSize: 22.sp,
+                        fontFamily: "serief",
+                        color: Colors.purple),
+                  ),
+                ),
+              ),
             ],
           ),
-          Text('Name: ${controller.bankAccounts[index].name}'),
-          Text('Balance: ${controller.bankAccounts[index].balance}'),
-          Text(
-              'Created: ${controller.bankAccounts[index].created.toString().substring(0, 19)}'),
-          Text('Description: ${controller.bankAccounts[index].description}'),
-          TextButton(
-            onPressed: () {
-              Get.to(
-                () => TurnoversListView(
-                  bankAccountId: controller.bankAccounts[index].id,
-                ),
-              );
-            },
-            child: Text('See Turnovers'),
-          ),
-        ],
+        ),
       );
     }
 
     return Scaffold(
       appBar: Constants.customAppBar(),
-      body: Center(
-        child: Constants.responsiveGlassBlock(
-          widthRatio: 0.9,
-          heightRatio: 0.8,
-          context: context,
-          content: block(),
-        ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/5390239.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Constants.responsiveGlassBlock(
+              widthRatio: 0.9,
+              heightRatio: 0.8,
+              context: context,
+              content: block(),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/views/home_views/detail_views/bill_detail_view.dart
+++ lib/views/home_views/detail_views/bill_detail_view.dart
@@ -6,6 +6,7 @@ import 'package:billing_app/enums/turnover_type.dart';
 import 'package:billing_app/models/bank_account.dart';
 import 'package:billing_app/models/turnover.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'package:get/get.dart';
 
 class BillDetailView extends StatefulWidget {
@@ -61,92 +62,120 @@ class _BillDetailViewState extends State<BillDetailView> {
     }
 
     block() {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text('Index: ${widget.index}'),
-              Text('Id: ${billsController.bills[widget.index].id}')
-            ],
-          ),
-          Text(
-              'Mount: ${billsController.bills[widget.index].mount.toString()} \$'),
-          Text(
-              'Creditor Name: ${billsController.bills[widget.index].creditorName}'),
-          Text(
-              'DeadLine: ${billsController.bills[widget.index].deadLine.toString().substring(0, 10)}'),
-          Text(
-              'Description: ${billsController.bills[widget.index].description}'),
-          billsController.bills[widget.index].isPayment
-              ? Text('already Paied!')
-              : Column(
-                  children: [
-                    _inputBankAccountIndex == null
-                        ? Container()
-                        : Text(bankAccountsController
-                            .bankAccounts[_inputBankAccountIndex].name),
-                    chosingBankAccount(),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_inputBankAccountIndex != null) {
-                          if (bankAccountsController
-                                  .bankAccounts[_inputBankAccountIndex]
-                                  .balance <
-                              billsController.bills[widget.index].mount) {
-                            billPaymentStatus = 'No enouf balance';
-                            setState(() {});
-                          } else {
-                            bankAccountsController
-                                .bankAccounts[_inputBankAccountIndex]
-                                .addingBalance(
-                                    -billsController.bills[widget.index].mount);
-                            bankAccountsController.updateBankAccount(
+      return Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Mount: ${billsController.bills[widget.index].mount.toString()} \$',
+              style: TextStyle(fontSize: 24.sp, fontFamily: "serief"),
+            ),
+            Text(
+              'Creditor: ${billsController.bills[widget.index].creditorName}',
+              style: TextStyle(fontSize: 20.sp, fontFamily: "serief"),
+            ),
+            Text(
+              'DeadLine: ${billsController.bills[widget.index].deadLine.toString().substring(0, 10)}',
+              style: TextStyle(fontSize: 16.sp, fontFamily: "dubay"),
+            ),
+            Text(
+              'Description: ${billsController.bills[widget.index].description}',
+              style: TextStyle(fontSize: 14.sp, fontFamily: "dubay"),
+            ),
+            billsController.bills[widget.index].isPayment
+                ? Text(
+                    'already Paied!',
+                    style: TextStyle(
+                        fontSize: 22.sp,
+                        fontFamily: "serief",
+                        color: Colors.green[800]),
+                  )
+                : Center(
+                    child: Column(
+                      children: [
+                        _inputBankAccountIndex == null
+                            ? Container(
+                                child: Text("No bank chosen Yet!"),
+                              )
+                            : Text(bankAccountsController
+                                .bankAccounts[_inputBankAccountIndex].name),
+                        chosingBankAccount(),
+                        ElevatedButton(
+                          onPressed: () {
+                            if (_inputBankAccountIndex != null) {
+                              if (bankAccountsController
+                                      .bankAccounts[_inputBankAccountIndex]
+                                      .balance <
+                                  billsController.bills[widget.index].mount) {
+                                billPaymentStatus = 'No enouf balance';
+                                setState(() {});
+                              } else {
                                 bankAccountsController
-                                        .bankAccounts[_inputBankAccountIndex]
-                                    as BankAccount);
-                            billsController.bills[widget.index]
-                                .changeIsPayment();
-                            billsController.updateBill(
-                                billsController.bills[widget.index]);
-                            turnoversController.createTurnover(
-                              Turnover(
-                                mount:
-                                    billsController.bills[widget.index].mount,
-                                bankAccountId: _inputBankAccountIndex == null
-                                    ? 0
-                                    : bankAccountsController
-                                        .bankAccounts[_inputBankAccountIndex]
-                                        .id,
-                                time: DateTime.now(),
-                                turnoverType: TurnoverType.Cost.index,
-                                description: 'bill paied!',
-                              ),
-                            );
-                            setState(() {});
-                          }
-                        } else
-                          print('Choose bank account!');
-                      },
-                      child: Text('Payment!'),
+                                    .bankAccounts[_inputBankAccountIndex]
+                                    .addingBalance(-billsController
+                                        .bills[widget.index].mount);
+                                bankAccountsController.updateBankAccount(
+                                    bankAccountsController.bankAccounts[
+                                        _inputBankAccountIndex] as BankAccount);
+                                billsController.bills[widget.index]
+                                    .changeIsPayment();
+                                billsController.updateBill(
+                                    billsController.bills[widget.index]);
+                                turnoversController.createTurnover(
+                                  Turnover(
+                                    mount: billsController
+                                        .bills[widget.index].mount,
+                                    bankAccountId:
+                                        _inputBankAccountIndex == null
+                                            ? 0
+                                            : bankAccountsController
+                                                .bankAccounts[
+                                                    _inputBankAccountIndex]
+                                                .id,
+                                    time: DateTime.now(),
+                                    turnoverType: TurnoverType.Cost.index,
+                                    description: 'bill paied!',
+                                  ),
+                                );
+                                setState(() {});
+                              }
+                            } else
+                              print('Choose bank account!');
+                          },
+                          child: Text('Payment!'),
+                        ),
+                        Text(billPaymentStatus),
+                      ],
                     ),
-                    Text(billPaymentStatus),
-                  ],
-                ),
-        ],
+                  ),
+          ],
+        ),
       );
     }
 
     return Scaffold(
       appBar: Constants.customAppBar(),
-      body: Center(
-        child: Constants.responsiveGlassBlock(
-          widthRatio: 0.9,
-          heightRatio: 0.8,
-          context: context,
-          content: block(),
-        ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/5390239.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Constants.responsiveGlassBlock(
+              widthRatio: 0.9,
+              heightRatio: 0.8,
+              context: context,
+              content: block(),
+            ),
+          ),
+        ],
       ),
     );
   }
